54fb20ab68ad36b42c3ff0011adbe3106f8301b7
erdnusskeks <erdnusskeks@googlemail.com>
2017-06-11 15:28:24 +0200

    Add picture uploader to toy model

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,4 +1,5 @@
 class Toy < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
+  mount_uploader :picture, PictureUploader
 end
